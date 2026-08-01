38521a2e19d142af466cf8100d8a1d99e9c1874a
arazdymaho <alex-razdym4048@mail.ru>
2026-08-01 19:40:45 +0300

    add getUserInput and convertCurrency functions

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import "fmt"
 
+// const usdEuro float64 = 0.87
+// const usdRub float64 = 79.26
+
 func main() {
-	const usdEuro float64 = 0.87
-	const usdRub float64 = 79.26
+	usdEuro, usdRub := getUserInput()
+	var euroRub float64 = usdRub / usdEuro
 
-	const euroRub float64 = usdRub / usdEuro
+	fmt.Println(euroRub)
+}
 
-	fmt.Println(euroRub) 
+func getUserInput() (usdEuro, usdRub float64) {
+	fmt.Print("Введите курс доллара к евро: ")
+	fmt.Scan(&usdEuro)
+	fmt.Print("Введите курс доллара к рублю: ")
+	fmt.Scan(&usdRub)
+	return
+}
+
+func convertCurrency(amount float64, from, to string) {
+
 }
