bbad139ba87c9a44403b3519545fb63fdf18f120
arazdymaho <alex-razdym4048@mail.ru>
2026-08-31 20:39:49 +0300

    add map for converCurrency

---
+++ 1-converter/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	UsdToRub = 79.4637
+	EurToRub = 91.1925
+	EurToUsd = 1.15325
+
+	UsdToEur = 0.867115
+	RubToUsd = 0.012584
+	RubToEur = 0.010966
+)
+
+type currency = map[string]float64
+
+
+func main() {
+	sourceCurrency := readCurrency("исходную")
+	destinationCurrency := readCurrency("целевую")
+	amount := readAmount(sourceCurrency)
+	currencies := currency{
+		"usdrub": 79.4637,
+		"eurrub": 91.1925,
+		"eurusd": 1.15325,
+		"usdeur": 0.867115,
+		"rubusd": 0.012584,
+		"rubeur": 0.010966,
+	}
+	convertCurrency(amount, sourceCurrency, destinationCurrency, currencies)
+}
+
+func readCurrency(role string) string {
+	var currency string
+	for {
+		fmt.Printf("Введите %s валюту USD/EUR/RUB: ", role)
+		fmt.Scan(&currency)
+		currency = strings.ToLower(strings.TrimSpace(currency))
+		if checkCurrency(currency) {
+			break
+		} else {
+			fmt.Println("Введенное значение не является валютой")
+			continue
+		}
+	}
+
+	return currency
+}
+
+func readAmount(baseCurrency string) float64 {
+	var amount float64
+	var input string
+	var err error
+	baseCurrency = strings.ToUpper(baseCurrency)
+	for {
+		fmt.Printf("Введите сумму в %s, которую хотите конвертировать: ", baseCurrency)
+		fmt.Scan(&input)
+		amount, err = strconv.ParseFloat(input, 64)
+		if err != nil {
+			fmt.Println("Вы ввели неправильное число. Попробуйте снова")
+			continue
+		} else {
+			break
+		}
+	}
+
+	return amount
+}
+
+func checkCurrency(currency string) bool {
+	return currency == "usd" || currency == "eur" || currency == "rub" 
+}
+
+func convertCurrency(amount float64, from, to string, currencies currency) {
+	key := from + to
+	fmt.Printf("Сумма %s в %s: %.2f\n", from, to, amount * currencies[key])
+}
